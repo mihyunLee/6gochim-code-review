3bcbc0999b8d9629627cebe78ed483cfb4231f91
jongmi jung <116716381+jjo-mi@users.noreply.github.com>
2023-05-04 18:39:01 +0900

    [JS100]문제27 _객체만들기

---
+++ JJM/JS100제/027.js
@@ -0,0 +1,17 @@
+// # 문제27 : 객체 만들기
+
+// 첫번째 입력에서는 학생의 이름이 공백으로 구분되어 입력되고, 두번째에는 그 학생의 수학 점수가 공백으로 구분되어 주어집니다.
+
+// 두 개를 합쳐 **학생의 이름이 key**이고 **value가 수학 점수**인 객체를 출력해주세요.
+
+
+const name = prompt('학생 두명 영어로 입력해주세요. 입력시 공백으로 구분! 🙏').split(' ');
+const score = prompt('수학 점수 공백을 기준으로 두 학생이 점수를 입력해주세요!').split(' ');
+                   
+const obj = {};
+
+for(let i =0; i< name.length; i++){
+  obj[name[i]] = parseInt(score[i]);
+};
+
+console.log(obj);
