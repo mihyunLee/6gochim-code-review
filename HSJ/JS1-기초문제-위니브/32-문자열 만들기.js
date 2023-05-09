7e724234bb10cf562a75834b220300218e1c612d
soooee4 <soooee4@gmail.com>
2023-05-09 20:49:34 +0900

    [JS] JS 100제 - Day10(031-032)

--- HSJ/JS1-기초문제-위니브/32-문자열 만들기.js
+++ HSJ/JS1-기초문제-위니브/32-문자열 만들기.js
@@ -1,5 +1,7 @@
 function wordCounter() {
-  const input = prompt("혜림아 자소서 적어봐 몇 글잔지 알려줄게 👀").split(" ");
+  const input = prompt("혜림아 자소서 적어봐 몇 글잔지 알려줄게! 👀").split(
+    " "
+  );
 
   const result = alert(parseInt(input.length));
 }
