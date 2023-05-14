6a459f7c5782a2f502f9404677a49cc0b3e25243
jongmi jung <116716381+jjo-mi@users.noreply.github.com>
2023-05-15 00:32:19 +0900

    fix_[JS100]문제37_반장 선거

--- JJM/JS100제/037.js
+++ JJM/JS100제/037.js
@@ -4,4 +4,27 @@
 // 그런데 표를 하나씩 개표하는 과정이 너무 번거롭게 느껴진 당신은 
 // 학생들이 뽑은 후보들을 입력받으면 뽑힌 학생의 이름과 받은 표 수를 출력하는 프로그램**을 작성하기로 하였습니다.
 
-fuction 
+function vote(input) {
+  let arr = input.split(" ");
+  let cnt = new Map();
+  for (let i of arr) {
+    if (cnt.has(i)) {
+      cnt.set(i, cnt.get(i) + 1);
+    } else {
+      cnt.set(i, 1);
+    }
+  }
+
+  let maxValue = Math.max(...cnt.values());
+  let winner = "";
+
+  cnt.forEach((values, key) => {
+    if (values == maxValue) {
+      winner = key;
+    }
+  });
+
+  console.log(`${winner}(이)가 총 ${maxValue}표로 반장이 되었습니다.`);
+}
+
+vote("원범 원범 혜원 혜원 혜원 혜원 유진 유진");
